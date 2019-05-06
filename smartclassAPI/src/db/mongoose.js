d7709c3846afafb9474f335f388f17de1c9d9e93
Arthur Pieri <arthurpieri@gmail.com>
2019-05-06 18:00:48 -0300

    Setting up the enviroment variables

--- smartclassAPI/src/db/mongoose.js
+++ smartclassAPI/src/db/mongoose.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose')
 
 // Starting mongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/smartclass', {
+// url mongodb://127.0.0.1:27017/smartclass
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
 // Only to avoid an alert: useFindAndModify is deprecated
