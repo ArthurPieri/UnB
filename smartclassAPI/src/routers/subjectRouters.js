cbb718d72793f0c1aa841f298254ad30e18d9feb
Arthur Pieri <arthurpieri@gmail.com>
2019-05-30 19:38:21 -0300

    Finishing CRUD routers for subject

--- smartclassAPI/src/routers/subjectRouters.js
+++ smartclassAPI/src/routers/subjectRouters.js
@@ -23,7 +23,7 @@ router.post('/subjects', async(req, res) => {
     }
 })
 
-// Private GET all subjects
+// Private READ all subjects
 // Reminder to change auth!
 router.get('/subjects/all', async (req, res) => {   
     try{
@@ -35,7 +35,7 @@ router.get('/subjects/all', async (req, res) => {
     
 })
 
-// Private GET one Subject
+// Private READ one Subject
 // Reminder to change auth!
 router.get('/subjects/:id', async (req, res) => {
     const _id = req.params.id
@@ -50,5 +50,47 @@ router.get('/subjects/:id', async (req, res) => {
         res.status(500).send()
     }
 })
+
+// Private UPDATE one subject
+router.patch('subjects/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['address', 'professors', 'enrollmentkey']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send('Erro: Atualização não permitida')
+    }
+
+    try{
+        const subject = await Subject.findOne({ _id})
+        if (!subject) {
+            return res.status(404).send()
+        }
+
+        updates.forEach((update) => subject[update] = req.body[update])
+        await subject.save()
+        res.send(subject)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+// Private DELETE one subject
+router.delete('/subjects/:id', async (req, res) => {
+    const _id = req.params.id
+    try{
+        const subject = await Subject.findOneAndDelete({_id})
+
+        if (!subject) {
+            return res.status(404).send()
+        }
+
+        res.send(subject)
+    }catch (e){
+        res.status(500).send(e)
+    }
+})
+
 
 module.exports = router
